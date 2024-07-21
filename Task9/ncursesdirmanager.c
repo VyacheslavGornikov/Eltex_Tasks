3683bc171946cb6daed1eb3509ed1b1ec5a75a1e
VyacheslavGornikov <slava98mail.ru@mail.ru>
2024-07-21 13:30:36 +0700

    Added sort in read_directory and / before directories

--- Task9/ncursesdirmanager.c
+++ Task9/ncursesdirmanager.c
@@ -1,7 +1,9 @@
 #include "ncursesdirmanager.h"
+#include <stdlib.h>
 #include <string.h>
 #include <dirent.h>
 #include <errno.h>
+#include <sys/stat.h>
 
 /*
  * Функция отображает в окне список файлов директории
@@ -12,7 +14,8 @@
  * selected - номер файла, выделенного курсором
  */
 void show_directory(WINDOW *wnd, char files[][256], int num_files, int selected)
-{
+{  
+    
     for (int i = 0; i < num_files; i++)
     {
         if (i == selected)
@@ -36,6 +39,7 @@ void read_directory(char files[][256], int* num_files, char* dir_path)
 {
     DIR* dir = NULL;
     struct dirent* ent;
+    struct stat st;
 
     *num_files = 0;
     if ((dir = opendir(dir_path)) != NULL) 
@@ -44,20 +48,67 @@ void read_directory(char files[][256], int* num_files, char* dir_path)
         {
             if (strcmp(ent->d_name, ".") != 0) 
             {
-                strcpy(files[*num_files], ent->d_name);
-                (*num_files)++;
-            }
+                char full_path[256];
+                snprintf(full_path, sizeof(full_path) * 2, "%s/%s", dir_path, ent->d_name);
+                
+                // Получаем информацию о файле с помощью stat
+                if (stat(full_path, &st) == 0) 
+                {
+                    // Проверяем тип объекта
+                    if (S_ISDIR(st.st_mode)) 
+                    { 
+                        // Это директория
+                        strncpy(files[*num_files], ent->d_name, 256 * sizeof(char));
+                        // Добавляем "/" в начало имени директории
+                        memmove(files[*num_files] + 1, files[*num_files], strlen(files[*num_files]) + 1); 
+                        files[*num_files][0] = '/';
+                    } 
+                    else  
+                    {
+                        // Это файл
+                        strncpy(files[*num_files], ent->d_name, 256 * sizeof(char)); 
+                    }
+                    (*num_files)++;
+                } 
+                else 
+                {
+                    clear();
+                    mvprintw(0, 0, "Error reading info from stat\n");
+                    printw("%s\n", strerror(errno));
+                    printw("Program finished. Press any key to exit...\n");                    
+                    getch();
+                    endwin();
+                    exit(EXIT_FAILURE);
+                }
+            }           
+
         }
         closedir(dir);
+
+        // Сортировка
+        for (int i = 0; i < *num_files - 1; i++) 
+        {
+            for (int j = i + 1; j < *num_files; j++) 
+            {
+                if (strcmp(files[i], files[j]) > 0) 
+                {
+                    char temp[256];
+                    strcpy(temp, files[i]);
+                    strcpy(files[i], files[j]);
+                    strcpy(files[j], temp);
+                }
+            }
+        }
     } 
     else 
     {
         clear();
         mvprintw(0, 0, "Error opening directory: %s\n", dir_path);
         printw("%s\n", strerror(errno));
-        strcpy(dir_path, "/home");
+        strncpy(dir_path, "/home", 256 * sizeof(char));
         read_directory(files, num_files, dir_path);
         printw("You will be returned to path /home. Press any key to continue...\n");
         getch();
     }
 }
+
